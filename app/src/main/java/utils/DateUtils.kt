a14cee700ebcbba5805ee375b1e3e45e03b2d0f7
Elif Buse Öztürk <116295101+Elofoloji8@users.noreply.github.com>
2024-12-26 12:25:40 +0300

    son

---
+++ app/src/main/java/utils/DateUtils.kt
@@ -0,0 +1,9 @@
+package com.todo.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun formatTimestamp(timestamp: Long): String {
+    val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+    return sdf.format(Date(timestamp))
+}
